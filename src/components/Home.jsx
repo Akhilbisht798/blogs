46d05856f529d619790316173c7b6a4589a1d183
Akhilbisht798 <akhilbisht798@gmail.com>
2024-06-30 16:14:53 +0530

    blogs rendered in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,26 @@
 import { Blog } from "./HomeComponents"
+import filesList from '../blogsList.json'
+import { useState } from "react";
 
 export default function Home() {
+    const [showAll, setShowAll] = useState(false);
+
+    function onClickHandler() {
+        setShowAll(!showAll);
+    }
+    
     return (
         <div className="">
-            Home
-            <div className="blogs">
-                <Blog title="ABC" filepath="/blog/abc.md"/>
-            </div>
-            <button>Show All</button>
+            <h1 className=" text-3xl">Home</h1>
+            {
+                filesList.files.map((item, index) => {
+                    if (index > 1 && !showAll) {
+                        return;
+                    }
+                    return <Blog title={item.title} key={index} filepath={`/blog/${item.filePath}`} />
+                })
+            }
+            <button onClick={onClickHandler}>Show All</button>
         </div>
     )
 }
